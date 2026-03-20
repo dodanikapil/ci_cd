60bf152bd5209d12cb47e9e34175121a82f1f7dc
dodanikapil <kapildodani@gmail.com>
2026-03-20 15:41:51 -0700

    initial_commit

---
+++ read_yaml.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import re
+import yaml
+#import func
+
+def reading_yaml(func):
+    def print_msg(*args, **kwargs):
+        print ("I am decorator")
+        return func(*args, **kwargs)
+    return print_msg
+
+#@reading_yaml
+def load_config(yaml_file):
+    with open(yaml_file,"r") as yml:
+        config = yaml.safe_load(yml)
+        #config = yaml.load_all(yml, Loader = yaml.SafeLoader)
+        #config = yaml.load_all(yml,Loader=yaml.SafeLoader)
+        return config
+
+print (__name__)
+
+if __name__ == "__main__":
+    yaml_file = "xrun.yaml"
+    data = load_config(yaml_file)
+
+    if data:
+        #print (data)
+        #print ("\n"*2,data.get(tool))
+        #print (data[0]["dic_check"]["name"][0])
+        print (data["tool"], data["xlm_version"], ' '.join(data["build_switches"]))
+        #print ("\n"*2+"KD"*5, data[1])
+       # print (data["Xcelium_version"])
+       # print (data.get("Xcelium_version"))
+       # for i in data.get("build_switches"):
+       #     print (i)
+       # print (data.get("str_check"))
+       # print (data.get("str_check2"))
+    
