5c443f5852d57f38f3d5edaab087eac834bff4bd
dodanikapil <kapildodani@gmail.com>
2026-03-20 17:03:52 -0700

    loading multiple yaml

--- read_yaml.py
+++ read_yaml.py
@@ -12,9 +12,9 @@ def reading_yaml(func):
 #@reading_yaml
 def load_config(yaml_file):
     with open(yaml_file,"r") as yml:
-        config = yaml.safe_load(yml)
+        #config = yaml.safe_load(yml)
         #config = yaml.load_all(yml, Loader = yaml.SafeLoader)
-        #config = yaml.load_all(yml,Loader=yaml.SafeLoader)
+        config = yaml.load_all(yml,Loader=yaml.SafeLoader)
         return config
 
 print (__name__)
@@ -23,11 +23,13 @@ if __name__ == "__main__":
     yaml_file = "xrun.yaml"
     data = load_config(yaml_file)
 
-    if data:
+    for f in data:
+        print (f)
+    if f:
         #print (data)
         #print ("\n"*2,data.get(tool))
         #print (data[0]["dic_check"]["name"][0])
-        print (data["tool"], data["xlm_version"], ' '.join(data["build_switches"]))
+        print (f["tool"], f["xlm_version"], ' '.join(f["build_switches"]))
         #print ("\n"*2+"KD"*5, data[1])
        # print (data["Xcelium_version"])
        # print (data.get("Xcelium_version"))
